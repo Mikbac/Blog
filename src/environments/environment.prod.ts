a179de08aa0f93d3e615ad50a06881e9331a3d1c
MikBac <mbachorz1997@gmail.com>
2020-09-26 01:49:16 +0200

    fixed travis config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
+import { mainConfiguration } from './configuration/configuration';
+import { firebaseConfiguration } from './firebase/firebase';
+
 export const environment = {
-  production: true
+    production: true,
+    firebase: firebaseConfiguration,
+    configuration: mainConfiguration,
 };
