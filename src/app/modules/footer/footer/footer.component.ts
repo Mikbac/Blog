5c15d69df6aadcf6afe563883f2e9d6c9ac13f74
MikBac <mbachorz1997@gmail.com>
2020-10-03 01:56:28 +0200

    fixed fortawesome import

--- src/app/modules/footer/footer/footer.component.ts
+++ src/app/modules/footer/footer/footer.component.ts
@@ -1,14 +1,11 @@
 import { ConfigurationService } from 'src/app/services/configuration.service';
 import { Component, OnInit } from '@angular/core';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-footer',
     templateUrl: './footer.component.html',
 })
 export class FooterComponent implements OnInit {
-    faCoffee = faCoffee;
-
     constructor(private configurationService: ConfigurationService) {}
 
     ngOnInit(): void {}
